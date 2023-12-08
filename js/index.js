e602802f0000e806e1ac28482da99582b9f41387
chrismartin5 <chrismartinunc@gmail.com>
2023-12-07 21:25:12 -0500

    hw19

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log("Your index.js file is loaded correctly!");
+$(".shopSmall img").mouseover(function() {
+    $(".shopSmall img").css({ width: '260px', height: 'auto' });
+});
+
+$(".shopSmall img").mouseout(function() {
+    $(".shopSmall img").css({ width: '250px', height: 'auto' });
+});
+
+$(".tailTalk img").mouseover(function() {
+    $(".tailTalk img").css({ width: '260px', height: 'auto' });
+});
+
+$(".tailTalk img").mouseout(function() {
+    $(".tailTalk img").css({ width: '250px', height: 'auto' });
+});
+
+$(".ncdsf img").mouseover(function() {
+    $(".ncdsf img").css({ width: '260px', height: 'auto' });
+});
+
+$(".ncdsf img").mouseout(function() {
+    $(".ncdsf img").css({ width: '250px', height: 'auto' });
+});
